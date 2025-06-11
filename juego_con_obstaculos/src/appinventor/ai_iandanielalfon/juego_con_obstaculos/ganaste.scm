3a9a930884a75b95885cc5df1e13aa38729d30e8
Ian Daniel Alfonso Rodriguez <iandanielalfon@gmail.com>
2025-06-11 07:57:42 +0200

    Add files via upload

---
+++ juego_con_obstaculos/src/appinventor/ai_iandanielalfon/juego_con_obstaculos/ganaste.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"232","Source":"Form","Properties":{"$Name":"ganaste","$Type":"Form","$Version":"31","AlignHorizontal":"3","AppName":"juego_con_obstaculos","BackgroundImage":"ganaste.gif","Theme":"Classic","Title":"ganaste","Uuid":"0","$Components":[{"$Name":"Bot\u00f3n1","$Type":"Button","$Version":"7","Text":"TRY AGAIN","Uuid":"386852331"}]}}
+|#
---
+++ juego_con_obstaculos/src/appinventor/ai_iandanielalfon/juego_con_obstaculos/perdiste.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"232","Source":"Form","Properties":{"$Name":"perdiste","$Type":"Form","$Version":"31","AlignHorizontal":"3","AppName":"juego_con_obstaculos","Theme":"Classic","Title":"perdiste","Uuid":"0","$Components":[{"$Name":"Bot\u00f3n1","$Type":"Button","$Version":"7","Text":"TRY AGAIN ","Uuid":"540475875"},{"$Name":"Imagen1","$Type":"Image","$Version":"6","Height":"-2","Width":"-1100","Picture":"perdiste.jpg","Uuid":"-1442551258"}]}}
+|#
